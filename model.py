239da97c4bcc834c7519097af4874bbfd8849acd
Rcurtain <r_curtain@163.com>
2018-01-11 16:43:06 +0800

    格式化代码

--- model.py
+++ model.py
@@ -2,13 +2,16 @@ import web
 
 table_name = 'todo'
 db_name = 'guest'
-db = web.database(dbn= 'mysql', db = db_name, user = 'guest', pw = 'guest')
+db = web.database(dbn='mysql', db=db_name, user='guest', pw='guest')
+
 
 def get_todos():
-	return db.select(table_name, order='id')
+    return db.select(table_name, order='id')
+
 
 def new_todo(text):
-	db.insert(table_name, title=text)
+    db.insert(table_name, title=text)
+
 
 def del_todo(id):
-	db.delete(table_name, where="id=$id", vars=locals())
+    db.delete(table_name, where="id=$id", vars=locals())
